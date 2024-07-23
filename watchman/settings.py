2c6ed6213825434a1cb4b930575b7c2e88afe1d0
muthomi254 <dmunene202020@gmail.com>
2024-07-23 08:16:24 +0300

    (Chor): updating allowed hosts

--- watchman/settings.py
+++ watchman/settings.py
@@ -39,8 +39,8 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('DEBUG', 'False') == 'True'
 
-# ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-ALLOWED_HOSTS= "*"
+ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+# ALLOWED_HOSTS= "*"
 
 
 # Application definition
